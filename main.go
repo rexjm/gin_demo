7c3e7d4f5e59de02312a301cbf06054789503776
Rex <1763814819@qq.com>
2023-01-09 11:17:15 +0800

    "win10test"

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -31,5 +30,5 @@ func main() {
 	//启动HTTP服务，默认在0.0.0.0:8080启动服务
 	r.Run()
 
-	fmt.Println("git test")
+	//fmt.Println("git test")
 }
